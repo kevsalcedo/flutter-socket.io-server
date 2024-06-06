b6dbc6b42dbc00af3c0eea47ade56450738853f1
kevsalcedo <kevsalcedom@gmail.com>
2024-06-05 20:48:38 -0500

    Cambios en las bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,15 +1,51 @@
-const {io} = require('../index')
+const { io } = require("../index");
+const Band = require("../models/band");
+const Bands = require("../models/bands");
+
+const bands = new Bands();
+
+bands.addBand(new Band("Green Day"));
+bands.addBand(new Band("Foo Fighters"));
+bands.addBand(new Band("Simple Plan"));
+bands.addBand(new Band("Blink 182"));
 
 //mensajes io de socket.io
 io.on("connection", (client) => {
-    console.log("Cliente conectado :D");
-    client.on("disconnect", () => {
-      console.log("Cliente desconectado :(");
-    });
-  
-    client.on("mensaje", (payload) => {
-      console.log("Mensaje!!", payload);
-  
-      io.emit("mensaje", { admin: "Nuevo mensaje?" });
-    });
+  console.log("Cliente conectado :D");
+
+  client.emit("active-bands", bands.getBands());
+
+  client.on("disconnect", () => {
+    console.log("Cliente desconectado :(");
   });
+
+  client.on("mensaje", (payload) => {
+    console.log("Mensaje!!", payload);
+    io.emit("mensaje", { admin: "Nuevo mensaje?" });
+  });
+
+  client.on("vote-band", (payload) => {
+    bands.voteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
+  });
+
+  client.on("add-band", (payload) => {
+    const newBand = new Band(payload.name)
+    bands.addBand(newBand);
+    io.emit("active-bands", bands.getBands());
+  });
+
+  client.on("delete-band", (payload) => {
+    bands.deleteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
+  });
+
+  client.on("emitir-mensaje", (payload) => {
+    //!Emite a todos!
+    //io.emit("nuevo-mensaje", payload);
+    //!Emite a todos menos el que lo emitio.
+    client.broadcast.emit("nuevo-mensaje", payload);
+
+    //console.log(payload);
+  });
+});
